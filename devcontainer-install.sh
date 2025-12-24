b30db35e3316de0079613e381cbd5e64784ad6eb
Tudor Timi <tudortimi@users.noreply.github.com>
2025-12-24 19:02:16 +0200

    Pass necessary configuration variables
    This is exactly what we wanted to avoid, by passing them as args to `dotfiles.installCommand`. Until this is allowed, we'll have to live with this workaround.

--- devcontainer-install.sh
+++ devcontainer-install.sh
@@ -23,7 +23,8 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-set -- init --apply --source="${script_dir}"
+set -- init --apply --source="${script_dir}" \
+		--promptString name="Tudor Timi",email="tudortimi@users.noreply.github.com"
 
 echo "Running 'chezmoi $*'" >&2
 # exec: replace current process with chezmoi
